70929086dc932ddd640c203dd96562e80f909669
Daniel Schüssler <daniels@community.haskell.org>
2012-02-03 11:22:44 +0100

    .gitignore

--- bench/driver.hs
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env runghc
-
-import System.Process
-import System.Exit
-
-main = do
-    ExitSuccess <- rawSystem "ghc" (words "main.hs -i.. -O2 -outputdir outputdir -fforce-recomp")
-    exitWith =<< rawSystem "./main" []
