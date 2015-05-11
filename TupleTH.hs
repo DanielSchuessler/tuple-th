c4ba9e80f5bd34b3c08e97cedf78941d5b42dc6c
Jakub Ryška <ryskajakub@seznam.cz>
2015-05-11 21:16:41 +0200

    add macro generating function updating single element in tuple

--- TupleTH.hs
+++ TupleTH.hs
@@ -7,7 +7,7 @@
 -- @ $('catTuples' 1 2) = \\x (y,z) -> (x,y,z) @
 module TupleTH(
     -- * Transformation
-        mapTuple, mapTuple', filterTuple, filterTuple', reindexTuple, reverseTuple, rotateTuple, subtuples, deleteAtTuple, takeTuple, dropTuple, safeDeleteTuple, 
+        mapTuple, mapTuple', filterTuple, filterTuple', reindexTuple, reverseTuple, rotateTuple, subtuples, deleteAtTuple, takeTuple, dropTuple, safeDeleteTuple, updateAtN,
     -- * Combination
         zipTuple, catTuples, uncatTuple, splitTupleAt,
     -- ** ZipWith
@@ -495,8 +495,16 @@ findSuccessiveElementsSatisfying n = do
                 [|Nothing|]))
                 
 
-
-
-            
-
-
+-- | generates function modifying single element in a tuple
+updateAtN :: Int -- ^ length of the input tuple
+          -> Int -- ^ index of the tuple starting with 0
+          -> Q Exp -- ^ (b -> c) -> (a1,a2,b,a3,a4) -> (a1,a2,c,a3,a4)
+updateAtN length element = do
+  mapFunction <- newName "x"
+  names <- forM [1..length] $ const $ newName "x"
+  let
+    parameters = map VarP names
+    results = map VarE names
+    (start, x:xs) = splitAt element results
+    finalResults = start ++ [AppE (VarE mapFunction) x] ++ xs
+  return $ LamE [VarP mapFunction] ( LamE [ TupP parameters ] $ TupE finalResults )
